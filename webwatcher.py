e88ce61731b36d50a2f4a3cfdd5b70b7a0414524
Martin Ingesen <martin@ingesen.no>
2018-10-31 16:38:53 +0100

    Some more pythonic code

--- webwatcher.py
+++ webwatcher.py
@@ -4,21 +4,22 @@ reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
 config = yaml.safe_load(open("config.yml"))
+SLEEP_TIME = 30
 
 if len(sys.argv) < 2:
-    print "[!] Need to supply a file with urls to watch."
-    print "[!] Example: python webwatcher.py /path/to/file.txt"
+    print("[!] Need to supply a file with urls to watch.")
+    print("[!] Example: python webwatcher.py /path/to/file.txt")
     sys.exit()
 
 def sendmail(url):
     datet = datetime.datetime.now().time()
-    datestring = "%s:%s" % (str(datet.hour), str(datet.minute))
+    datestring = "{}:{}".format(str(datet.hour), str(datet.minute))
 
     FROM = config['username']
     TO  = config['recipients']
     SUBJECT = config['subject'] % datestring
-    BODY = "There was a change in the url " + url + " at " + datestring
-    MESSAGE = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (FROM, TO, SUBJECT, BODY)
+    BODY = "There was a change in the url {} at {}".format(url, datestring)
+    MESSAGE = "FROM: {}\nTo: {}\nSubject: {}\n\n{}".format(FROM, TO SUBJECT, BODY)
 
     username = config['username']
     password = config['password']
@@ -30,32 +31,31 @@ def sendmail(url):
     server.quit()
 
 # READ FILE
-print "[+] Reading file"
+print("[+] Reading file")
 urls = []
 
 with open(sys.argv[1], 'r') as fil:
     urls = fil.read().strip().split("\n")
 
-
 # PREPROCESS HASHES
-print "[+] Preprocessing hashes"
+print("[+] Preprocessing hashes")
 hashes = []
 
 for url in urls:
-    print "    [-] " + url
+    print("\t[-] {}".format(url))
     r = requests.get(url, cookies=config["cookies"]).text
     hashes.append(hashlib.md5(r).hexdigest())
 
 # START LOOKING FOR CHANGES
-print "[*] Looking for changes"
-while 1:
+print("[*] Looking for changes")
+while True:
     for index, url in enumerate(urls):
         r = requests.get(url, cookies=config["cookies"]).text
-        hasj = hashlib.md5(r).hexdigest()
-        if hasj != hashes[index]:
-            # SHIT BANDIT SEND AN EMAIL
+        hash = hashlib.md5(r).hexdigest()
+        if hash != hashes[index]:
+            # Detected a change, sending mail
             sendmail(url)
-            print "[!] We noted a change in the url " + url
-            hashes[index] = hasj
+            print("[!] We noted a change in the url {}".format(url))
+            hashes[index] = hash
 
-    time.sleep(30)
+    time.sleep(SLEEP_TIME)
